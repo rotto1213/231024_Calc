f00aa1137358d8a6499ea8e57ef5f3f11603a084
Ybawan <92610152+Ybawan@users.noreply.github.com>
2023-10-25 19:16:42 +0000

    test

--- test/test_controller.py
+++ test/test_controller.py
@@ -1,10 +1,11 @@
+@pytest.mark.parameterize("a,b,c", [(1, 2, 3), (3, 6, 9)])
 def test_returnSignal(controller):
     """Tests the Return key binding interface to our Qt display widget."""
     from PyQt5 import QtCore, QtGui
 
-    controller._view.setDisplayText("1+2")
+    controller._view.setDisplayText("{}+{}".format(a, b))
     event = QtGui.QKeyEvent(
         QtCore.QEvent.KeyPress, QtCore.Qt.Key_Enter, QtCore.Qt.NoModifier
     )
     controller._view.display.keyPressEvent(event)
-    assert controller._view.displayText() == "3"
+    assert controller._view.displayText() == "{}".format(c)
